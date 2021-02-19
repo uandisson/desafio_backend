ab645507b84fe6efe241d873b90aa5d165af6ac8
Uandisson Miranda <uandisson@gmail.com>
2021-02-19 13:54:18 -0300

    add test_action.py

---
+++ backend-python/credito-express-app/test_action.py
@@ -0,0 +1,10 @@
+import os
+import requests
+
+def main():
+    test_output = f"Crédito Express APP"
+
+    print(f"::set-output name=testOutput::{test_output}")
+
+if __name__ == "__main__":
+    main()
